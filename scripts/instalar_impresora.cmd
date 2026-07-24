e5ecb460b14a71f24ea618be921dfe8e1ef81e78
luisgutierrezco <luisdavidgutierrezco@gmail.com>
2026-07-24 11:16:08 -0400

    vault sync 2026-07-24T15:16:06.852Z

--- scripts/instalar_impresora.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnport.vbs -a -r "IPP_PORT" -h 192.168.92.21 -o raw -n 631
-cscript %WINDIR%\System32\Printing_Admin_Scripts\en-US\prnmngr.vbs -a -p "AUDITORIA EPSON" -m "EPSON L3250 Series" -r "http://192.168.92.21:631/printers/AUDITORIA_EPSON"
-echo Listo.
